f2aa989da878e264bb4d6776b63837cefe789118
Shohruh Masharipov <shohruhmasharipov49@gmail.com>
2021-01-27 01:07:19 +0500

    work time

---
+++ lib/details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Page'),
+      ),
+      body: Container(),
+    );
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_navigation/details_screen.dart';
 
 class HomePage extends StatelessWidget {
+  void openDetails(BuildContext ctx) {
+    Navigator.push((ctx), MaterialPageRoute(builder: (_) {
+      return DetailsPage();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            FlatButton(
+              onPressed: () => openDetails(context),
+              child: Text('Click me'),
+              color: Colors.red,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
